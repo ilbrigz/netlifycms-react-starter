10b7a38db8ba5b219372672d65695c168fe7f412
Brayn <ilogirb23@yahoo.com>
2018-11-13 17:21:06 +0800

    .

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,9 +5,10 @@ import Markdown from 'react-markdown'
 import logoImg from '../logo.png'
 
 export default withRouteData(({home}) => (
+
   <div>
-    <h1 style={{ textAlign: 'center' }}>{home.data.title}</h1>
-    <img src={home.data.image} alt="" style={{ display: 'block', margin: '0 auto' }} />
+    <h1 style={{ textAlign: 'center' }}>{home}</h1>
+
     <Markdown source={post.content} escapeHtml={false} />
   </div>
 ))
