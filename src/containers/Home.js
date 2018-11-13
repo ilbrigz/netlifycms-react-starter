39421fcb252febf6f18f635d6a47e2663422c278
Brayn <ilogirb23@yahoo.com>
2018-11-13 19:44:44 +0800

    '.'

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,7 +9,7 @@ export default withRouteData(({home}) => {
 const {content, data} =(home[0])
 return (
 
-  <div>
+  <div style={backgroundColor:"red"}>
   <h1 style={{ textAlign: 'center' }}>test</h1>
   <img src={data.image }alt=""/>
  <Markdown source={content} escapeHtml={false} />
