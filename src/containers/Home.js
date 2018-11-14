898acccb79cf05ee860f584605d1b9ab4bd93d93
Brayn <ilogirb23@yahoo.com>
2018-11-14 14:58:55 +0800

    .

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,12 +6,16 @@ import {FacebookIcon,FacebookShareButton} from 'react-share';
 import logoImg from '../logo.png'
 
 export default withRouteData(({home}) => {
-
+const siteUrl = () => {
+	if (typeof window !== 'undefined') {
+ return window.location.href
+	}
+}
 const {content, data} =(home[0])
 return (
 
   <div>
-  <FacebookShareButton quote="the great brigs" url={document.URL}><FacebookIcon></FacebookIcon></FacebookShareButton>
+  <FacebookShareButton quote="the great brigs" url={siteUrl}><FacebookIcon></FacebookIcon></FacebookShareButton>
   <h1 style={{ textAlign: 'center' }}>test</h1>
   <img src={data.image }alt=""/>
  <Markdown className="center is-100"  source={content} escapeHtml={false} />
