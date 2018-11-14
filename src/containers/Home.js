df8d7c57f81dc588cb87ab6b5471818a996a0cee
Brayn <ilogirb23@yahoo.com>
2018-11-14 16:26:10 +0800

    .

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,9 +8,10 @@ import logoImg from '../logo.png'
 export default withRouteData(({home}) => {
 const {content, data} =(home[0])
 return (
-{typeof window !== 'undefined' && <FacebookShareButton url={window.location.href}
-	><FacebookIcon></FacebookIcon></FacebookShareButton>}
+
   <div>
+  {typeof window !== 'undefined' && <FacebookShareButton url={window.location.href}
+	><FacebookIcon></FacebookIcon></FacebookShareButton>}
   <h1 style={{ textAlign: 'center' }}>test</h1>
   <img src={data.image }alt=""/>
  <Markdown className="center is-100"  source={content} escapeHtml={false} />
