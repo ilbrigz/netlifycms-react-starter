237550dd943c01c3df62c3f454fd1cd9531f822e
Brayn <ilogirb23@yahoo.com>
2018-11-14 14:50:23 +0800

    .

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
 import Markdown from 'react-markdown'
+import {FacebookIcon,FacebookShareButton} from 'react-share';
 //
 import logoImg from '../logo.png'
 
@@ -9,7 +10,8 @@ export default withRouteData(({home}) => {
 const {content, data} =(home[0])
 return (
 
-  <div style={{backgroundColor:"red"}}>
+  <div>
+  <FacebookShareButton quote="the great brigs" url={window.location.href }><FacebookIcon></FacebookIcon></FacebookShareButton>
   <h1 style={{ textAlign: 'center' }}>test</h1>
   <img src={data.image }alt=""/>
  <Markdown className="center is-100"  source={content} escapeHtml={false} />
